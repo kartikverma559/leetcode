bfd35fad87be1e226ce864b3ac877979b9b5fa55
Kartik Verma <kartikverma559@gmail.com>
2026-09-08 10:50:47 +0530

    Time: 2 ms (94.76%) | Memory: 47.7 MB (34.58%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n=nums.length;
+        int[] arr=new int[2];
+        int[] freq=new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            freq[nums[i]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(freq[i]>1) arr[0]=i;
+            if(freq[i]==0) arr[1]=i;
+        }
+        return arr;
+    }
+}
